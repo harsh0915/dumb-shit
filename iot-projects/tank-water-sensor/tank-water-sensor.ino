e75d2b7d5fc2e90eaf734d5d37c17e15162db060
harsh0915 <harshmehra0915@gmail.com>
2026-07-29 15:14:05 +0530

    implemented mqtt for the water sensor

--- iot-projects/tank-water-sensor/tank-water-sensor.ino
+++ iot-projects/tank-water-sensor/tank-water-sensor.ino
@@ -1,39 +1,56 @@
-int trigPin = 13;
-int echoPin = 5;
+#include "EspMQTTClient.h"
+
+int trig_pin = 13;       
+int echo_pin = 5;
 int duration;
-float currentDistance;
-int totalDistance = 11;
-float waterLevelPercentage;
+float distance;
+int total_distance = 11;
+float water_level_percentage;
+
+EspMQTTClient client(
+  "Harsh",
+  "Test123!",
+  "harsh-raspberry-pi-0wh.local",  // broker
+  "WaterSensor",     // unique client name 
+  1883              // mqtt port, default to 1883
+);
 
 void setup() {
-  //Serial Port begin
-  Serial.begin (9600);
+  Serial.begin(9600);
+  client.enableDebuggingMessages(); 
   //Define inputs and outputs
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
 }
 
+void onConnectionEstablished()
+{
+  Serial.println("Connection established!");
+  // Publish a message to "mytopic/test"
+  client.publish("sensors/water-sensor", "55"); // You can activate the retain flag by setting the third parameter to true
+}
  
 void loop() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
+  // the sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trig_pin, LOW);
 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echo_pin, HIGH,26000); // read in times pulse
+  distance= duration/58;//convert to cm
+  Serial.print(distance);
+  Serial.println(" cm");
+  // Wait 50ms before next ranging
+  delay(50);
 
-  // Convert the time into a distance
-  currentDistance = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343 for values in cm
+  water_level_percentage=((float) (total_distance-distance)/(float)total_distance)*100;
+  Serial.println(distance);
+  String message = String(distance)+" cm";
 
-  Serial.print("Percentage filled : ");
-//  Serial.print(currentDistance);
-  Serial.print(((float) (totalDistance-currentDistance)/(float)totalDistance)*100);
-  Serial.println();
+  client.publish("sensors/water-sensor", message);
+
+  client.loop();
 }
